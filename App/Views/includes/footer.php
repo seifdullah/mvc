6654a4152944bd5034b4dc03f2190eb2a35de5fb
seifdullah <hazemanasfa6@gmail.com>
2022-09-02 10:55:03 +0300

    changed footer

--- App/Views/includes/footer.php
+++ App/Views/includes/footer.php
@@ -1,5 +1,7 @@
 <footer class="pt-5 my-5 text-muted border-top">
-    Created by The Legendary Seif; &copy; <script type="text/javascript">
+    Created by The Legendary Seif
+    &copy;
+    <script type="text/javascript">
         var year = new Date();
         document.write(year.getFullYear());
     </script>
